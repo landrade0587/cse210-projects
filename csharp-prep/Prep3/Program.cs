1387229eca478b05ddbe5f63534f722c4553316e
landrade0587 <and23002@byui.edu>
2023-06-11 03:18:38 -0500

    added prep 3 numbers

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //We declared the variable  that will control
+        // the do while loop
+        String contPlaying = "";
+        do{
+            //We have to create the variables that we will use inside of the do while
+            // and while loopss also this will restarts the variable if the user wants to play again
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,11);
+            int inpNumber = 0;
+            String inputTwo;
+            int tries = 0;
+
+            //the loop tha will be the one control  the game itself
+            while (magicNumber != inpNumber)
+            {
+                tries++;
+                Console.Write("What is your guess? ");
+                inputTwo = Console.ReadLine();
+                inpNumber = int.Parse(inputTwo);
+
+                if (inpNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");  
+                }
+
+                else if(inpNumber < magicNumber)
+                {
+                    Console.WriteLine("Higther");
+                }
+
+                else 
+                {
+                    Console.WriteLine("You guessed it! ");
+                    Console.WriteLine($"It tooked you {tries} tries! ");
+                    Console.Write("Do you want to comtinue ? ");
+                    contPlaying =Console.ReadLine();
+                }
+
+
+            }
+        } while(contPlaying == "yes");
     }
 }
