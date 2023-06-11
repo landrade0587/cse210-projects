d753194436cdcb567083ba8e8e817c63dc2ed7b9
landrade0587 <and23002@byui.edu>
2023-06-11 02:38:53 -0500

    added Prep 2 prepotition activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please Introduce your percentage grade: ");
+        string userInput = Console.ReadLine();
+        int gradePer = int.Parse(userInput);
+        string letter;
+
+        if (gradePer >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradePer >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (gradePer >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (gradePer >= 60)
+        {
+            letter = "D";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        if (gradePer % 10 >= 7 && !(letter == "A" || letter == "F"))
+        {
+            letter +="+";
+        }
+
+        else if (gradePer % 10 < 3 && letter != "F")
+        {
+            letter += "-";
+        }
+
+
+        //printing the grade in letter
+        Console.WriteLine($"Your grade is {letter}");
+
+        // Now We Will aadd conditionals to determine
+        // if the user passed or not
+
+        if (gradePer >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the course. !!");
+            
+        } 
+
+        else
+        {
+            Console.WriteLine("Sorry  you have not approved, although if you try harder you will do it next) time !!");
+        }
     }
 }
