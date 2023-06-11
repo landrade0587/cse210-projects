749b4c16873af7ce90467bed880e6c8564c1d87d
landrade0587 <and23002@byui.edu>
2023-06-11 01:42:10 -0500

    Prep 1 added the new code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {  
+       //User imput added
+       Console.Write("What is your first name:? ");
+       string first_name = Console.ReadLine();
+
+       Console.Write("What is your last name:? ");
+       string last_name = Console.ReadLine();   
+
+       //Added the output
+       Console.WriteLine($"Your name is: {last_name}, {first_name} {last_name}.");        
     }
 }
