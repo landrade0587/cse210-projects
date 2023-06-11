f001d818e4354acdfceeefd45fa73c1ac4d561ed
landrade0587 <and23002@byui.edu>
2023-06-11 03:55:32 -0500

    added prep 4 y 5 final

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Declaring variables and a message
+        List<int> numbers = new List<int>();
+        int numberInp = -1;
+        double average = 0;
+        int highestNumber = 0;
+        double total = 0;
+        int lowerNumber = 999999999;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        //do while loop to ask the user for numbers that will be add to the list 
+        //until the user types in 0
+        do{
+
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            numberInp = int.Parse(input);
+
+            //conditional to add the numbers to the list without including 0 
+            if (numberInp != 0) {
+                numbers.Add(numberInp);
+            }
+
+        }while(numberInp != 0);
+
+        //foreach loop to get total sum of list's numbers
+        //also I used this loop to get the higher and lower number of the list 
+        foreach (int number in numbers){
+            total += number;
+            if (number > highestNumber){
+                highestNumber = number;
+            }
+            if (number < lowerNumber && number > 0){
+                lowerNumber = number;
+
+            }
+        }
+
+        //calculating the average
+        average = total/numbers.Count;
+
+        //Sorting the list in ascending way
+        numbers.Sort();
+
+        //Displaying the messages to the console
+        Console.WriteLine($"The sum is {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest is: {highestNumber}");
+        Console.WriteLine($"The smallest positive number is: {lowerNumber}");
+        
+        //Displaying all the list in ascending way 
+        foreach (int number in numbers){
+            Console.WriteLine(number);
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Declaring variables with the values returns for the functions,
+        //likewise calling the functions that don't return anything
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int numberSquared = SquareNumber(favoriteNumber);
+
+        DisplayResult(name, numberSquared);
+
+        //Functions
+        static void DisplayWelcome(){
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName(){
+            Console.Write("Please enter your name: ");
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        static int PromptUserNumber(){
+            Console.Write("Please enter your favorite number: ");
+            string input = Console.ReadLine();
+            int favoriteNumber = int.Parse(input);
+            return favoriteNumber;
+        }
+
+        static int SquareNumber(int number){
+            int numberSquared = number * number;
+            return numberSquared;
+        }
+
+        static void DisplayResult(string name, int numberSquared){
+            Console.WriteLine($"{name}, the square of your number is {numberSquared}");
+        }
     }
 }
