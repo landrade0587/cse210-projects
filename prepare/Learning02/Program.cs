dd55677360888fb7530573a6536a8dcefd042d92
landrade0587 <and23002@byui.edu>
2023-06-13 10:12:24 -0500

    added preparation excercise end

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,14 +7,14 @@ class Program
         Job job1 = new Job();
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
-        job1._startYear = 2005;
-        job1._endYear = 2010;
+        job1._startYear = 2013;
+        job1._endYear = 2018;
 
         Job job2 = new Job();
         job2._company = "Apple";
         job2._jobTitle = "Database Engineer";
-        job2._startYear = 2015;
-        job2._endYear = 2022;
+        job2._startYear = 2018;
+        job2._endYear = 2023;
 
         job1.DisplayJobDetails();
         job2.DisplayJobDetails();
